b42d96c8fe8ee5a7ac9d6f4d847af0af1731c28b
unknown <dangvu0502@gmail.com>
2022-07-26 21:25:08 +0700

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// styles
+import "./App.css";
+
+// page components
+import Navbar from "./components/Navbar";
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipe" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
