9021c6d83a316eb4b0e849cfca81167b9a97d2fd
unknown <dangvu0502@gmail.com>
2022-07-28 20:39:15 +0700

    Update Create component and add Searchbar

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,6 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 
 // styles
 import "./Create.css";
@@ -10,11 +12,28 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState("");
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
+  const navitage = useNavigate();
+
+  const { postData, data, error } = useFetch(
+    "http://localhost:3000/recipes",
+    "POST"
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime, ingredients);
+    postData({
+      title,
+      ingredients,
+      method,
+      cookingTime: cookingTime + " minutes",
+    });
   };
+
+  useEffect(() => {
+    if (data) {
+      navitage("/");
+    }
+  }, [data, navitage]);
 
   const handleAdd = (e) => {
     e.preventDefault();
